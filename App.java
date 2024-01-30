30ffb5afc50d47ff32a66f060b33bc1b059a6a98
gatoplay12 <aaron.garlop.1@educa.jcyl.es>
2024-01-30 10:06:00 +0100

    primer commit

---
+++ App.java
@@ -0,0 +1,13 @@
+package git.prollecto1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
